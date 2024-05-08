dad447ae10bcd0d656dd58f3eefc4d6c505fa16c
admin <cokimson900@gmail.com>
2024-05-08 11:51:53 +0700

    add vidu1

--- app/src/main/java/com/example/lab1_ph30902_ktl/vidu1.kt
+++ app/src/main/java/com/example/lab1_ph30902_ktl/vidu1.kt
@@ -5,9 +5,11 @@ import android.net.IpPrefix
 fun printMessage(message: String): Unit {
     println(message)
 }
+
 fun printMessageWithPrefix(message: String = "cc", prefix: String = "Info") {
     println("[$prefix] $message")
 }
+
 /*
 1. `message` là một chuỗi (String) đại diện cho nội dung thông điệp mà bạn muốn in ra màn hình.
 2. `prefix` cũng là một chuỗi (String), mặc định là "Info". Đối số này đại diện cho tiền tố mà bạn muốn thêm vào trước nội dung thông điệp.
@@ -15,6 +17,7 @@ fun printMessageWithPrefix(message: String = "cc", prefix: String = "Info") {
 fun sum(x: Int, y: Int): Int {
     return x + y
 }
+
 fun multiply(x: Int, y: Int) = x * y
 fun main() {
     printMessage("xin chao")
@@ -24,5 +27,6 @@ fun main() {
     println(sum(1, 2))
     println(multiply(2, 4))
 }
+
 class vidu1 {
 }
